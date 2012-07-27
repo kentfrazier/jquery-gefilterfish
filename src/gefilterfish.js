b449bde492dd45299a3b8f45e105b76e2f0c54b6
Kent Frazier <kentfrazier@gmail.com>
2012-07-27 16:10:33 -0500

    Set up initial project structure with grunt.js. Also, I suck at git

---
+++ src/gefilterfish.js
@@ -0,0 +1,28 @@
+/*
+ * gefilterfish
+ * https://github.com/kentfrazier/jquery-gefilterfish
+ *
+ * Copyright (c) 2012 Kent Frazier
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
